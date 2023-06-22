a998202a2f9778b44ed65c44906d137978f2c1bb
kfrier0 <116312828+kfrier0@users.noreply.github.com>
2023-06-21 19:40:30 -0500

    assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+// VARIABLES
+// let, var, const
+
+//console.log(1); // Print to the Console
+const name = "pizza";
+
+const topping = "pineapple";
+/* adding two strings;
+contcatenation */
+console.log(name, topping);
+// TEMPLATE LITERALS
+console.log(`${name} ${topping}`);
+
+console.log("32" + "1000" + "str");
+// integer
+
+//booleans
+// false, undefined,null, 0
+let num = undefined;
+
+console.log(Boolean(num));
+fkoe;
+
+let prompt1 = prompt("What is your name?");
+let HowOldAreYou = prompt("How old are you?");
+let WhatIsYourHobby = prompt("What is your hobby?");
+alert(`${prompt1} is ${HowOldAreYou} and like ${WhatIsYourHobby}`);
